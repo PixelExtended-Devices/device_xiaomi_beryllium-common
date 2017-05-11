e822d941bf976265e3333c7c9449463eb6ffc985
Wei Wang <wvw@google.com>
2017-05-11 14:06:36 -0700

    sdm845-common:Override heap growth limit to 256M

--- sdm845.mk
+++ sdm845.mk
@@ -380,3 +380,6 @@ PRODUCT_PACKAGES += \
 # msm_irqbalance
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+# Override heap growth limit
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=256m
